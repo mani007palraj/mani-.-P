8b4421f29887792dfea4f322cf4afccc71be23c7
mani007palraj <51704819+mani007palraj@users.noreply.github.com>
2019-06-12 16:53:32 +0530

    Update vowel.py

--- CODEKATA/vowel.py
+++ CODEKATA/vowel.py
@@ -1,10 +1,9 @@
-a=input()
-a=a.lower()
-v=("aeiou")
-x=("bcdfghjklmnpqrstvwxyz")
-if(a in v):
-  print("Vowel")
-elif(a in x):
-  print("Consonant")
+s=input()
+v=("a,e,i,o,u")
+a=("b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z")
+if(s in v):
+  print('Vowel')
+elif(s in a):
+  print('Consonent')
 else:
-  print("invalid")
+  print('Invalid')
